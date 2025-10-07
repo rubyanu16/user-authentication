73f41fd4d7665a42f05f401e04b100116ee5b735
rubyanu16 <rubyanusuya098@gmail.com>
2025-10-07 09:26:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const API_URL = "http://localhost:5000/api/auth";
+
+async function register() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch(`${API_URL}/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, email, password })
+  });
+
+  const data = await res.json();
+  document.getElementById("message").innerText = data.message;
+}
+
+async function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password })
+  });
+
+  const data = await res.json();
+  document.getElementById("message").innerText = data.message;
+}
